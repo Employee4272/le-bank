525495af43edd74cfb801416080c1e8ef9fce882
Employee4272 <alekskozak12@gmail.com>
2023-12-15 11:18:29 +0100

    Add project files.

---
+++ le bank/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace le_bank
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.Main = new System.Windows.Forms.TextBox();
+            this.by10 = new System.Windows.Forms.CheckBox();
+            this.by100 = new System.Windows.Forms.CheckBox();
+            this.div100 = new System.Windows.Forms.CheckBox();
+            this.div10 = new System.Windows.Forms.CheckBox();
+            this.minusRandom = new System.Windows.Forms.CheckBox();
+            this.plusRandom = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.a_bank = new System.Windows.Forms.TextBox();
+            this.i_bank = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.d_bank = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_aleks = new System.Windows.Forms.Label();
+            this.lbl_ivan = new System.Windows.Forms.Label();
+            this.lbl_dima = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 43);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(107, 50);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "+";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(12, 99);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(107, 50);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "-";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(12, 217);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(107, 50);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "+";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(12, 492);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(107, 50);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "-";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(12, 436);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(107, 50);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "+";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 273);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(107, 50);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "-";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Main
+            // 
+            this.Main.Location = new System.Drawing.Point(12, 597);
+            this.Main.Name = "Main";
+            this.Main.Size = new System.Drawing.Size(712, 26);
+            this.Main.TabIndex = 6;
+            this.Main.TabStop = false;
+            this.Main.Text = "0";
+            this.Main.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // by10
+            // 
+            this.by10.AutoSize = true;
+            this.by10.Location = new System.Drawing.Point(0, 25);
+            this.by10.Name = "by10";
+            this.by10.Size = new System.Drawing.Size(59, 24);
+            this.by10.TabIndex = 7;
+            this.by10.Text = "*10";
+            this.by10.UseVisualStyleBackColor = true;
+            // 
+            // by100
+            // 
+            this.by100.AutoSize = true;
+            this.by100.Location = new System.Drawing.Point(0, 55);
+            this.by100.Name = "by100";
+            this.by100.Size = new System.Drawing.Size(68, 24);
+            this.by100.TabIndex = 8;
+            this.by100.Text = "*100";
+            this.by100.UseVisualStyleBackColor = true;
+            // 
+            // div100
+            // 
+            this.div100.AutoSize = true;
+            this.div100.Location = new System.Drawing.Point(0, 115);
+            this.div100.Name = "div100";
+            this.div100.Size = new System.Drawing.Size(75, 24);
+            this.div100.TabIndex = 10;
+            this.div100.Text = "1/100";
+            this.div100.UseVisualStyleBackColor = true;
+            // 
+            // div10
+            // 
+            this.div10.AutoSize = true;
+            this.div10.Location = new System.Drawing.Point(0, 85);
+            this.div10.Name = "div10";
+            this.div10.Size = new System.Drawing.Size(66, 24);
+            this.div10.TabIndex = 9;
+            this.div10.Text = "1/10";
+            this.div10.UseVisualStyleBackColor = true;
+            // 
+            // minusRandom
+            // 
+            this.minusRandom.AutoSize = true;
+            this.minusRandom.Location = new System.Drawing.Point(0, 175);
+            this.minusRandom.Name = "minusRandom";
+            this.minusRandom.Size = new System.Drawing.Size(159, 24);
+            this.minusRandom.TabIndex = 12;
+            this.minusRandom.Text = "-Random<1;100>";
+            this.minusRandom.UseVisualStyleBackColor = true;
+            this.minusRandom.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // plusRandom
+            // 
+            this.plusRandom.AutoSize = true;
+            this.plusRandom.Location = new System.Drawing.Point(0, 145);
+            this.plusRandom.Name = "plusRandom";
+            this.plusRandom.Size = new System.Drawing.Size(163, 24);
+            this.plusRandom.TabIndex = 11;
+            this.plusRandom.Text = "+Random<1;100>";
+            this.plusRandom.UseVisualStyleBackColor = true;
+            this.plusRandom.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 574);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "wpisz kwote";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Stan konta Aleks";
+            // 
+            // a_bank
+            // 
+            this.a_bank.Location = new System.Drawing.Point(0, 54);
+            this.a_bank.Name = "a_bank";
+            this.a_bank.Size = new System.Drawing.Size(225, 26);
+            this.a_bank.TabIndex = 15;
+            // 
+            // i_bank
+            // 
+            this.i_bank.Location = new System.Drawing.Point(0, 111);
+            this.i_bank.Name = "i_bank";
+            this.i_bank.Size = new System.Drawing.Size(225, 26);
+            this.i_bank.TabIndex = 17;
+            this.i_bank.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Stan konta Ivan";
+            // 
+            // d_bank
+            // 
+            this.d_bank.Location = new System.Drawing.Point(1, 172);
+            this.d_bank.Name = "d_bank";
+            this.d_bank.Size = new System.Drawing.Size(225, 26);
+            this.d_bank.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Stan konta Dima";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lbl_aleks
+            // 
+            this.lbl_aleks.AutoSize = true;
+            this.lbl_aleks.Location = new System.Drawing.Point(12, 9);
+            this.lbl_aleks.Name = "lbl_aleks";
+            this.lbl_aleks.Size = new System.Drawing.Size(48, 20);
+            this.lbl_aleks.TabIndex = 20;
+            this.lbl_aleks.Text = "Aleks";
+            // 
+            // lbl_ivan
+            // 
+            this.lbl_ivan.AutoSize = true;
+            this.lbl_ivan.Location = new System.Drawing.Point(12, 186);
+            this.lbl_ivan.Name = "lbl_ivan";
+            this.lbl_ivan.Size = new System.Drawing.Size(39, 20);
+            this.lbl_ivan.TabIndex = 21;
+            this.lbl_ivan.Text = "Ivan";
+            // 
+            // lbl_dima
+            // 
+            this.lbl_dima.AutoSize = true;
+            this.lbl_dima.Location = new System.Drawing.Point(12, 401);
+            this.lbl_dima.Name = "lbl_dima";
+            this.lbl_dima.Size = new System.Drawing.Size(46, 20);
+            this.lbl_dima.TabIndex = 22;
+            this.lbl_dima.Text = "Dima";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.a_bank);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.i_bank);
+            this.groupBox1.Controls.Add(this.d_bank);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(584, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 220);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Le Bank";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.by10);
+            this.groupBox2.Controls.Add(this.by100);
+            this.groupBox2.Controls.Add(this.div10);
+            this.groupBox2.Controls.Add(this.div100);
+            this.groupBox2.Controls.Add(this.plusRandom);
+            this.groupBox2.Controls.Add(this.minusRandom);
+            this.groupBox2.Location = new System.Drawing.Point(584, 251);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(344, 201);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Panel modyfikacji wartosci";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 635);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_dima);
+            this.Controls.Add(this.lbl_ivan);
+            this.Controls.Add(this.lbl_aleks);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Main);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.TextBox Main;
+        private System.Windows.Forms.CheckBox by10;
+        private System.Windows.Forms.CheckBox by100;
+        private System.Windows.Forms.CheckBox div100;
+        private System.Windows.Forms.CheckBox div10;
+        private System.Windows.Forms.CheckBox minusRandom;
+        private System.Windows.Forms.CheckBox plusRandom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox a_bank;
+        private System.Windows.Forms.TextBox i_bank;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox d_bank;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_aleks;
+        private System.Windows.Forms.Label lbl_ivan;
+        private System.Windows.Forms.Label lbl_dima;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ le bank/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace le_bank
+{
+    public partial class Form1 : Form
+    {
+        int result;
+        int stan_a;
+        int stan_b;
+        int stan_c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int modyfikant()
+        {
+            Random rnd = new Random();
+            result = Convert.ToInt32(Main.Text);
+            int a = result;
+            if (by10.Checked)
+            {
+                a *=10;
+            }
+            if(by100.Checked)
+            {
+                a*=100;
+            }
+            if (div10.Checked)
+            {
+                a /= 10;
+            }
+            if (div100.Checked)
+            {
+                a/=100;
+            }
+            if (plusRandom.Checked)
+            {
+                a = a + rnd.Next(1, 100);
+            }
+            if (minusRandom.Checked)
+            {
+                a = a - rnd.Next(1, 100);
+            }
+            return a;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stan_c -= modyfikant();
+            d_bank.Text = stan_c.ToString();
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            stan_c += modyfikant();
+            d_bank.Text = stan_c.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            stan_b -= modyfikant();
+            i_bank.Text = stan_b.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stan_b += modyfikant();
+            i_bank.Text = stan_b.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stan_a -= modyfikant();
+            a_bank.Text = stan_a.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stan_a += modyfikant();
+            a_bank.Text = stan_a.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
